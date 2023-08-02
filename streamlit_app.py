94b679009c7e3ca3039eef29268d45c4f3cd8f4e
Adarsh-Kumar7 <94464700+Adarsh-Kumar7@users.noreply.github.com>
2023-08-02 19:20:17 +0530

    Update streamlit_app.py (Added the Breakfast Menu Title)
    Added the title breakfast menu before the menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Omega 3 and Blueberry Oatmeal, Kale Spinach and Rocket Smoothie, Hard-Boiled free-range eggs')
+streamlit.title('Breakfast Menu, Omega 3 and Blueberry Oatmeal, Kale Spinach and Rocket Smoothie, Hard-Boiled free-range eggs')
