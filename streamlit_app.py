35702d20cfb52d2fab51102df57940eabab55107
Adarsh-Kumar7 <94464700+Adarsh-Kumar7@users.noreply.github.com>
2023-08-02 19:23:17 +0530

    Update streamlit_app.py (Tried New Line function)
    Applied Python New Line Command

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast Menu, Omega 3 and Blueberry Oatmeal, Kale Spinach and Rocket Smoothie, Hard-Boiled free-range eggs')
+streamlit.title('Breakfast Menu\n, Omega 3 and Blueberry Oatmeal\n, Kale Spinach and Rocket Smoothie\n, Hard-Boiled free-range eggs')
