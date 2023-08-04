f92d4f22749b8eb44729908427e1d39f611e28a8
Adarsh-Kumar7 <94464700+Adarsh-Kumar7@users.noreply.github.com>
2023-08-04 16:31:55 +0530

    Update streamlit_app.py(Making the JSON look good)
    Showing in table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display FruityVice api respone
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#streamlit.text(fruityvice_response.json())
 # FruityVice data JSON value normalized 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output table
